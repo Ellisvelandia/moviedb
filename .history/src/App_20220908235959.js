dd71fc252b317674bcef1abb4c052f0cfe469b9a
ellisvelandia <eyis619@gmail.com>
2022-09-09 00:00:53 -0500

    moviedb

--- .history/src/App_20220908235959.js
+++ .history/src/App_20220908235959.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
-import MovieList from "./components/movieList/MovieList";
 
 function App() {
   return (
@@ -12,7 +11,7 @@ function App() {
         <Routes>
           <Route index element={ <Home /> }></Route>
           <Route path="movie/:id" element={<h1>Movie detail page</h1>}></Route>
-          <Route path="movies/:type" element={<MovieList/>}></Route>
+          <Route path="movies/:type" element={</>}></Route>
           <Route path="/*" element={<h1>Error page</h1>}></Route>
         </Routes>
       </Router>
--- .history/src/App_20220909000005.js
+++ .history/src/App_20220909000005.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
-import MovieList from "./components/movieList/MovieList";
 
 function App() {
   return (
